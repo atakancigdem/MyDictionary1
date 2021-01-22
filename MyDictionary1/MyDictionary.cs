c34b42f7a91c8c19e01f1df6e5c6fdce3f94e4b8
JupiteRus <atakancigdem7@outlook.com>
2021-01-22 18:59:31 +0300

    Add project files.

---
+++ MyDictionary1/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary1
+{
+    class MyDictionary<TFirst , TLast, TAge>
+    {
+        TFirst[] FirstName;
+        TLast[] LastName;
+        TAge[] Age;
+        public MyDictionary()
+        {
+            FirstName = new TFirst[0];
+            LastName = new TLast[0];
+            Age = new TAge[0];
+        }
+        public void Add(TFirst firstName, TLast lastName, TAge age)
+        {
+            TFirst[] tempFirstName = FirstName;
+            TLast[] tempLastName = LastName;
+            TAge[] tempAge = Age;
+                        
+            FirstName = new TFirst[FirstName.Length + 1];
+            LastName = new TLast[LastName.Length + 1];
+            Age = new TAge[Age.Length + 1];
+
+            for (int i = 0; i < tempFirstName.Length; i++)
+            {
+                FirstName[i] = tempFirstName[i];
+                LastName[i] = tempLastName[i];
+                Age[i] = tempAge[i];
+            }
+
+            FirstName[FirstName.Length - 1] = firstName;
+            LastName[LastName.Length - 1] = lastName;
+            Age[Age.Length - 1] = age;
+        }
+
+        public int Length
+        {
+            get { return FirstName.Length; }
+        }
+
+        public TFirst[] Tfirsts
+        {
+            get { return FirstName; }
+        }
+
+        public TLast[] Tlast
+        {
+            get { return LastName; }
+        }
+        
+        public TAge[] Tage
+        {
+            get { return Age;}
+        }
+    }
+}
---
+++ MyDictionary1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyDictionary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string, int> MyList = new MyDictionary<string, string, int>();
+            MyList.Add("Atakan", "Çiğdem", 19);
+            MyList.Add("Melek", "Gül", 46);
+            MyList.Add("Furkan", "Çiğdem", 29);
+            int i = 0;
+
+            foreach (var firstNames in MyList.Tfirsts)
+            {
+                i = i + 1;
+                Console.WriteLine("Kişiler {0}. Adı : {1}" , i, firstNames);  
+            }
+            i = 0;
+            foreach (var lastNames in MyList.Tlast)
+            {
+                i = i + 1;
+                Console.WriteLine("Kişiler {0}. Soyadı : {1} ", i, lastNames);
+            }
+            i = 0;
+            foreach (var age in MyList.Tage)
+            {
+                i = i + 1;
+                Console.WriteLine("Kişiler {0}. Soyadı : {1} ", i, age);
+            }
+        }
+    }
+}
